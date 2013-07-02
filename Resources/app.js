1328fcbcef2e5ebda6a3b312ae9f6e7ebabaf56c
pedroramos <pedroframos@outlook.com>
2013-07-02 14:53:43 -0300

    Add Tests

--- Resources/app.js
+++ Resources/app.js
@@ -1,13 +1,13 @@
 bountyHunter = {};
 bountyHunter.osname = Ti.Platform.osname;
-bountyHunter.isAndroid = bountyHunter.osname === 'android'; 
+bountyHunter.isAndroid = bountyHunter.osname === 'android';
 bountyHunter.isIOS = bountyHunter.osname === 'iphone';
 
 (function() {
-	Titanium.UI.setBackgroundColor('gray');
-	
+	Titanium.UI.setBackgroundColor("gray");
+
 	var ApplicationTabGroup = require("ui/common/ApplicationTabGroup");
-	
+
 	var tabGroup = new ApplicationTabGroup();
 	tabGroup.open();
 })();
---
+++ spec/test_spec.js
@@ -0,0 +1,9 @@
+describe("Teste teste", function() {
+	it("Fail", function () {
+		expect(false).ToEqual(true);
+	})
+	
+	it("Success", function () {
+		expect(true);
+	})
+})
